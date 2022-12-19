5daf515c650eb5f3297f67a45ae950541860c8b7
rgm <rgm@ik.me>
2022-12-19 18:03:57 +0100

    Decimal display

--- src/feed.jsx
+++ src/feed.jsx
@@ -3,12 +3,24 @@ import './App.css';
 
 const Feed = ({coin}) => {
 
-    const price = parseFloat(coin.current_price).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " EUR";
+    // const price = parseFloat(coin.current_price).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    const priceFloat = parseFloat(coin.current_price); 
+    let price = ''; let priceInt = ''; let priceDec = '';
     const price_change = parseFloat(coin.price_change_percentage_24h);
     const color_price_change = price_change>0 ? 'green' : 'red';
     const price_change_string = price_change>0 ? 
         "+" + price_change.toFixed(1).toString() + ' %' :
         price_change.toFixed(1).toString() + ' %';
+    const bigPrice = coin.current_price>10;
+
+    if(bigPrice){
+        let priceIntFloat = Math.floor(priceFloat);
+        let priceDecFloat = priceFloat-priceIntFloat;
+        
+        priceInt = priceIntFloat.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        priceDec = priceDecFloat.toString().slice(0,4);
+    }
+    else{price = parseFloat(coin.current_price).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");}
 
     return(    
         <>  
@@ -19,7 +31,7 @@ const Feed = ({coin}) => {
                 <b>{` ${coin.name} (${coin.symbol.toUpperCase()})`}</b>
             </label>
             <br />
-            <font size="+1">{price}</font><b><label style={{color: color_price_change}}>{price_change_string}</label></b>
+            <font size="+2">{bigPrice? `${priceInt}.` : price}</font><font size="3">{priceDec}</font><font size="+2">{" €"}</font><b><label style={{color: color_price_change}}>{` ${price_change_string}`}</label></b>
             <br /><br />
         </>
     )
